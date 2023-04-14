1dff0a51198b884b5511a7897687487721e4678c
Leonardo <kjamanba@hotmail.com>
2023-04-14 10:01:28 -0300

    release-1.1

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,7 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
-const connectDB = async () => {
-  await mongoose.connect(process.env.DB_CONNECT,()=>console.log('connnect to db!')
-  )
+const connectDB = (url) => {
+  return mongoose.connect(url)
 }
 
 module.exports = connectDB
--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
+require('dotenv').config()
 const express = require("express")
 const app = express()
-const dotenv = require("dotenv")
-dotenv.config()
-const connnectDB = require("./config/db")
-connnectDB()
+const connectDB = require("./config/db")
 const cors = require("cors")
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 
+app.use('/',require("./routes/routes"))
 
-app.use("/",require("./routes/routes"))
+const port = process.env.PORT || 5000
 
+const start = async()=>{ 
+  try{
+    await connectDB(process.env.MONGO_URL)
+    app.listen(port,()=>
+      console.log('Server is running on port 5000')
+    )
+  } catch(error){
+    console.log(error)
+  }
+}
 
-const PORT = process.env.PORT || 5001
-
-app.listen(PORT,()=>console.log("Server is running")) 
+start()
